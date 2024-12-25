bba5b912809aaa7aca6dbeca9d3ade7d5032c802
webjmdesign000 <webjmdesign000@gmail.com>
2024-12-25 12:48:10 -0500

    Create config.php

---
+++ Config/config.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'name'        => 'Multi-Owner Bundle',
+    'description' => 'Add a campaign action that assigns multiple owners to a contact.',
+    'version'     => '1.0',
+    'author'      => 'webjmDesign0000',
+
+    'services' => [
+        'events' => [
+            'my_multi_owner.campaign_subscriber' => [
+                'class'     => \MauticPlugin\MyMultiOwnerBundle\EventListener\CampaignSubscriber::class,
+                'arguments' => [
+                    'mautic.user.model.user',
+                    'doctrine.orm.entity_manager',
+                ],
+            ],
+        ],
+        'other' => [
+            // You can define services or helper classes if needed
+        ],
+    ],
+
+    'campaigns' => [
+        'actions' => [
+            'my_multi_owner.campaign.action.assign_multiple_owners' => [
+                'description' => 'plugin.my_multi_owner.assign_multiple_owners.action',
+                'eventName'   => 'my_multi_owner.campaign.action.assign_multiple_owners',
+                'label'       => 'Assign Multiple Owners',
+                'formType'    => 'my_multi_owner_assign_multiple_owners',
+            ],
+        ],
+    ],
+];
